e48a6318dcd05ec2977834460ff2b13e16b28587
nikshep <klimenko.leva.lol2@yandex.ru>
2026-04-21 18:18:03 +0800

    feat: add new fonts and imports in root

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,8 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./App.tsx";
 import { AppProviders } from "./providers/AppProviders.tsx";
+import { App } from "@/app/App.tsx";
+
+import "@/styles/reset.css";
+import "@/styles/themes.css";
+import "@/styles/styles.css";
+import "@/styles/variables.css";
+
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
+import "@fontsource/inter/800.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
