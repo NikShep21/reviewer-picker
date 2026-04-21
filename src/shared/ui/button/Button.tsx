b1631532f756812b1cf6c82ecfb1ecd60d6ba233
nikshep <klimenko.leva.lol2@yandex.ru>
2026-04-21 18:07:55 +0800

    feat: add custom button

---
+++ src/shared/ui/button/Button.tsx
@@ -0,0 +1,40 @@
+import type { ButtonHTMLAttributes, PropsWithChildren } from "react";
+import clsx from "clsx";
+
+import { Spinner } from "@/shared/ui/spinner";
+
+import styles from "./Button.module.css";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>, PropsWithChildren {
+  variant?: "primary" | "secondary";
+  isLoading?: boolean;
+}
+
+export const Button = ({
+  children,
+  className,
+  disabled,
+  variant = "primary",
+  isLoading = false,
+  ...props
+}: ButtonProps) => {
+  return (
+    <button
+      className={clsx(styles.button, styles[variant], className)}
+      disabled={disabled || isLoading}
+      {...props}
+    >
+      <span className={styles.content}>
+        {isLoading && (
+          <span className={styles.spinner}>
+            <Spinner size={"md"} />
+          </span>
+        )}
+
+        <span className={clsx(styles.label, isLoading && styles.labelHidden)}>
+          {children}
+        </span>
+      </span>
+    </button>
+  );
+};
